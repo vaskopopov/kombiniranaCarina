1c700a3fe08c9ba2b3c99cfdc4a53f93bf77e5f3
Vasko Popov <57752249+vaskopopov@users.noreply.github.com>
2022-02-07 18:44:54 +0100

    app js

---
+++ app.js
@@ -0,0 +1,9 @@
+const articlePrice=$('.articlePrice');
+const customsPercent=$('.customsPercent');
+const customsValue=document.getElementsByName('customsValue');
+
+$(function(){
+
+    customsValue[0].value=50;
+
+});
